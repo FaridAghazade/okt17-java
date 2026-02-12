0b1e35786ff04ec5f7993f2d9e67532736e461de
FaridAghazade <gunay.agayeva01@gmail.com>
2026-02-12 18:11:53 +0100

    all java codes ferid

---
+++ src/okt17Java/Main.java
@@ -0,0 +1,13 @@
+package okt17Java;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		String t = "hello";
+		  System.out.println(t.length());
+		
+		
+	}
+
+}
---
+++ src/okt17Java/VariablesPractice.java
@@ -0,0 +1,10 @@
+package okt17Java;
+
+public class VariablesPractice {
+
+	public static void main(String[] args) {
+	int age = 20;
+          System.out.println("age:" + age );
+	}
+
+}
