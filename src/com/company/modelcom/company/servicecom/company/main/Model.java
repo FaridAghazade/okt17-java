30adc4c374a789bb8c8430daed4eab92d331670f
FaridAghazade <gunay.agayeva01@gmail.com>
2026-05-28 22:15:21 +0200

    ders34

---
+++ src/com/company/modelcom/company/servicecom/company/main/Model.java
@@ -0,0 +1,9 @@
+package com.company.modelcom.company.servicecom.company.main;
+
+public class Model {
+public static void main(String[] args) {
+	Student s = new Student("Ferid",13);
+	s.javanameagegetInfo();
+}
+
+}
---
+++ src/com/company/modelcom/company/servicecom/company/main/Student.java
@@ -0,0 +1,22 @@
+package com.company.modelcom.company.servicecom.company.main;
+
+public class Student {
+  String name;
+int age;
+
+
+public Student(String name, int age ) {
+	this.name = name;
+	this.age = age;
+
+}
+
+
+ public void javanameagegetInfo(){
+	 System.out.println("sagird("+"sagirdin adi: "+ name + ", "+ "sagirdin soy adi: "+ age + ")");
+ }
+
+}
+
+
+
