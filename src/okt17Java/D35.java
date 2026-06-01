a1b1cea8ee26ae65a57b947ec67b84bff08ef3c1
FaridAghazade <gunay.agayeva01@gmail.com>
2026-06-01 20:55:41 +0200

    all

---
+++ src/okt17Java/D35.java
@@ -0,0 +1,79 @@
+package okt17Java;
+
+public class D35 {
+ public void salam() {
+	 System.out.println("salam dunya");
+ }
+ 
+ //tap2
+ 
+ public void xettcek() {
+	 System.out.println("------------------------------");
+ }
+ 
+ //tap3
+ 
+ public void melumatGoster() {
+	 System.out.println("""
+	 ferid
+	 12
+	 Berlin
+	 	""");
+ }
+ 
+ 
+	 public void topla(int a , int b) {
+		 System.out.println(a+"+" + b + "=" + a+b);
+	 
+ }
+	 public void cix(int a , int b) {
+		 System.out.println(a-b);
+	 
+ }
+	 
+	 public void vur(int a , int b) {
+		 System.out.println(a+"*" + b + "=" + a*b);
+	 
+ }
+	 public void bol(int a , int b) {
+		 System.out.println(a+"/" + b + "=" + a/b);
+	 
+ }
+	 
+	 public void menugoster() {
+		 System.out.println("""
+		       ====================
+			      1. Oyuna başla
+			      2. Parametrlər
+			      3. Çıxış
+			      ====================
+			main-dən çağırın.
+		 		""");
+	 
+ }
+	 
+	 public void nece(boolean d) {
+		 int c = 0;
+		 if (d== true) {
+			c++;
+		}
+		 System.out.println("bu metod "+ c + " defe ise dusdu");
+	 
+ } 
+	  
+	 public void basla() {
+		 System.out.println("program basladi...");
+	 
+ }
+	 
+	 public void isle() {
+		 System.out.println("program isleyir...");
+	 
+ }
+	 
+	 public void bitir() {
+		 System.out.println("program bitdi...");
+	 
+ }
+ 
+}
---
+++ src/okt17Java/Main5.java
@@ -0,0 +1,33 @@
+package okt17Java;
+
+public class Main5 {
+
+	public static void main(String[] args) {
+           D35 d = new D35();
+           d.salam();
+           System.out.println();
+           d.xettcek();
+           System.out.println();
+           d.melumatGoster();
+           System.out.println();
+           d.topla(12,34);
+           System.out.println();
+           d.vur(2,4);
+           System.out.println();
+           d.cix(12,5);
+           System.out.println();
+           d.bol(12,4);
+           System.out.println();
+           d.menugoster();
+           System.out.println();
+           d.nece(true);
+           System.out.println();
+           d.basla();
+           System.out.println();
+           d.isle();
+           System.out.println();
+           d.bitir();
+           
+	}
+
+}
